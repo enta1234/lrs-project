5601a668c2df5b7054115a240ba4b794a0af2995
Uxcode <you@example.com>
2017-10-23 19:13:32 +0700

    template homepage

---
+++ application/views/welcome.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v4.3.4, # -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.3.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/rlpd-logo-125x140.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>หน้าแรก</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/data-tables/data-tables.bootstrap4.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/animate.css/animate.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+ 
+</head>
+<body>
+<!-- nav -->
+<section class="menu cid-qyLFvClZQ0" once="menu" id="menu1-m" data-rv-view="1285">
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="#top">
+                        <img src="assets/images/rlpd-logo-125x140.png" alt="Mobirise" title="" media-simple="true" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap">
+                    <a class="navbar-caption text-primary display-4" href="#top">กรมคุ้มครองสิทธิและเสรีภาพ <br>กระทรวงยุติธรรม</a>
+                </span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                <li class="nav-item">
+                    <a class="nav-link link text-warning display-4" href="index.html#table1-s">ข่าวสาร</a>
+                </li>
+                <li class="nav-item dropdown open">
+                    <a class="nav-link link text-warning dropdown-toggle display-4" data-toggle="dropdown-submenu" aria-expanded="true">คลินิกยุติธรรม</a>
+                    <div class="dropdown-menu">
+                        <div class="dropdown open">
+                            <a class="text-warning dropdown-item dropdown-toggle display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="true">เขตพื้นที่ 1</a>
+                            <div class="dropdown-menu dropdown-submenu">
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o">คลินิก 1</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o">คลินิก 2</a>
+                            </div>
+                        </div>
+                        <div class="dropdown">
+                            <a class="text-warning dropdown-item dropdown-toggle display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="false">เขตพื้นที่ 2</a>
+                            <div class="dropdown-menu dropdown-submenu">
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o">New Item</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o" aria-expanded="true">New Item</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o" aria-expanded="true">New Item</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o" aria-expanded="true">New Item</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o" aria-expanded="true">New Item</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o" aria-expanded="true">New Item</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o" aria-expanded="true">New Item</a>
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o" aria-expanded="true">New Item</a>
+                            </div>
+                        </div>
+                        <div class="dropdown">
+                            <a class="text-warning dropdown-item dropdown-toggle display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="false">เขตพื้นที่ 3</a>
+                            <div class="dropdown-menu dropdown-submenu">
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o">New Item</a>
+                            </div>
+                        </div>
+                        <div class="dropdown">
+                            <a class="text-warning dropdown-item dropdown-toggle display-4" href="#" data-toggle="dropdown-submenu" aria-expanded="false">เขตพื้นที่ 4</a>
+                            <div class="dropdown-menu dropdown-submenu">
+                                <a class="text-warning dropdown-item display-4" href="index.html#map1-o">New Item</a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-warning display-4" href="index.html#footer1-r">ติดต่อเรา</a>
+                </li>
+            </ul>
+            <div class="navbar-buttons mbr-section-btn">
+                <a class="btn btn-sm btn-primary display-4" href="#">สมัคร</a>
+            </div>
+        </div>
+    </nav>
+</section>
+<!-- intro -->
+<section class="header12 cid-qyLEW6FN40 mbr-fullscreen mbr-parallax-background" id="intro" data-rv-view="1214">
+    <div class="mbr-overlay" style="opacity: 0.9; background-color: rgb(15, 118, 153);"></div>
+    <div class="container  ">
+            <div class="media-container">
+                <div class="col-md-12 align-center">
+                    <h1 class="mbr-section-title pb-3 mbr-white mbr-bold mbr-fonts-style display-1"><br>สมัครเป็นที่ปรึกษาทางกฏหมายได้ทีนี่</h1>
+                    <p class="mbr-text pb-3 mbr-white mbr-fonts-style display-5">
+                        Full screen intro with background image, color overlay and icons
+                    </p>
+                    <div class="mbr-section-btn align-center py-2">
+                        <a class="btn btn-md btn-secondary display-4" href="#">สมัคร</a>
+                    </div>
+                    <div class="icons-media-container mbr-white">
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                            <a href="#/">
+                                <span class="mbri-layers mbr-iconfont" media-simple="true"></span>
+                            </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">
+                                Multi Layout
+                            </h5>
+                        </div>
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                <a href="#/">
+                                    <span class="mbri-sun mbr-iconfont" media-simple="true"></span>
+                                </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">
+                                Creative Idea
+                            </h5>
+                        </div>
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                <a href="#/">
+                                    <span class="mbri-cash mbr-iconfont" media-simple="true"></span>
+                                </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">
+                                Target Buyers
+                            </h5>
+                        </div>
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                <a href="#/">
+                                    <span class="mbri-mobirise mbr-iconfont" media-simple="true"></span>
+                                </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">
+                                Reach Top
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+<!-- new --> TODO: Change new template.
+<section class="section-table cid-qyLHCxqxRz" id="table1-s" data-rv-view="1217">
+  <div class="container container-table">
+      <h2 class="mbr-section-title mbr-fonts-style align-center pb-3 display-2">ข่าวสาร</h2>
+      <div class="table-wrapper">
+        <div class="container"></div>
+        <div class="container scroll">
+            <table class="table" cellspacing="0">
+                <thead>
+                    <tr class="table-heads ">  
+                        <th class="head-item mbr-fonts-style display-7">NAME</th>
+                        <th class="head-item mbr-fonts-style display-7">NAME</th>
+                        <th class="head-item mbr-fonts-style display-7">NAME</th>
+                        <th class="head-item mbr-fonts-style display-7">NAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr> 
+                        <td class="body-item mbr-fonts-style display-7">Jeanna Schmal</td>
+                        <td class="body-item mbr-fonts-style display-7">Jeanna Schmal</td>
+                        <td class="body-item mbr-fonts-style display-7">Jeanna Schmal</td>
+                        <td class="body-item mbr-fonts-style display-7">Jeanna Schmal</td>
+                    </tr>
+                    <tr>
+                        <td class="body-item mbr-fonts-style display-7">Caren Rials</td>
+                        <td class="body-item mbr-fonts-style display-7">Caren Rials</td>
+                        <td class="body-item mbr-fonts-style display-7">Caren Rials</td>
+                        <td class="body-item mbr-fonts-style display-7">Caren Rials</td>
+                    </tr>
+                    <tr>
+                        <td class="body-item mbr-fonts-style display-7">Leon Rogol</td>
+                        <td class="body-item mbr-fonts-style display-7">Leon Rogol</td>
+                        <td class="body-item mbr-fonts-style display-7">Leon Rogol</td>
+                        <td class="body-item mbr-fonts-style display-7">Leon Rogol</td>
+                    </tr>
+                    <tr>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                    </tr>
+                    <tr>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                        <td class="body-item mbr-fonts-style display-7">Shala Barrera</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="container table-info-container"></div>
+      </div>
+    </div>
+</section>
+<!-- map -->
+<section class="map1 cid-qyLGkhjoi4" id="map1-o" data-rv-view="1287">
+    <div class="google-map">
+        <iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0Dx_boXQiwvdz8sJHoYeZNVTdoWONYkU&amp;q=place_id:ChIJvRfnSCOD4jARmc5CEnSzGzM" allowfullscreen=""></iframe>
+    </div>
+</section>
+<!-- footer -->
+<section class="cid-qyLGxCggNy" id="footer1-r" data-rv-view="1289">
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="#/">
+                        <img src="assets/images/rlpd-logo-125x140.png" alt="Mobirise" title="" media-simple="true">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">ที่อยู่สำนักงานใหญ่</h5>
+                <p class="mbr-text">เลขที่ 120 หมู่ 3 ชั้น 3 ศูนย์ราชการเฉลิมพระเกียรติ 80 พรรษาฯ
+                                    <br>อาคารราชบุรี ดิเรกฤทธิ์ (อาคาร เอ) ถนนแจ้งวัฒนะ แขวงทุ่งสองห้อง เขตหลักสี่ กรุงเทพฯ 10210</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">ติดต่อ</h5>
+                <p class="mbr-text">
+                    โทรศัพท์ สายด่วน 1111 กด 77<br>
+                    โทรสาร. 02-143-9681<br>
+                    LINE ID : rlpdconsultation<br>
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">Links</h5>
+                <p class="mbr-text">
+                    <a class="text-primary" href="#/">Website builder</a>
+                    <br><a class="text-primary" href="#/mobirise-free-win.zip">Download for Windows</a>
+                    <br><a class="text-primary" href="#/mobirise-free-mac.zip">Download for Mac</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- script -->
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/smooth-scroll.js"></script>
+  <script src="assets/jarallax/jarallax.min.js"></script>
+  <script src="assets/viewport-checker/jquery.viewportchecker.js"></script>
+  <script src="assets/data-tables/jquery.data-tables.min.js"></script>
+  <script src="assets/data-tables/data-tables.bootstrap4.min.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touch-swipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+<!-- btn-go-top -->
+    <div id="scrollToTop" class="scrollToTop mbr-arrow-up">
+        <a style="text-align: center;">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+    <input name="animation" type="hidden">
+  </body>
+</html>
